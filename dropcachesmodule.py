f583889dd64df16a79ebcc333b6ad01387b33c69
Alex Nevi <acerlmt@gmail.com>
2023-07-30 11:34:47 +0700

    add drop caches every half hour

---
+++ dropcachesmodule.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import os
+
+class dropcachesmodule:
+
+    @staticmethod
+    def drop_caches():
+        print("sync && echo 1 > /proc/sys/vm/drop_caches && sync && echo 2 > /proc/sys/vm/drop_caches && sync && echo 3 > /proc/sys/vm/drop_caches")
+        os.system('sync && echo 1 > /proc/sys/vm/drop_caches && sync && echo 2 > /proc/sys/vm/drop_caches && sync && echo 3 > /proc/sys/vm/drop_caches')
--- ramer.py
+++ ramer.py
@@ -36,6 +36,9 @@ from printPosition.printPosition import printPosition as printp
 from pulseaudiomodule import PAmodule
 from notifymodule import notifymodule
 from argsmodule import argsmodule
+from dropcachesmodule import dropcachesmodule
+
+from timeit import default_timer as timer
 
 class RAMer:
 
@@ -54,51 +57,44 @@ class RAMer:
     @staticmethod
     def run(self):
 
+        count = 0
         drop_caches_count = 0
         debug = 0
         priority = ['discord', '/code/code', 'brave/brave', 'brave-beta', 'sublime_text']
 
         if 'SUDO_UID' in os.environ: self.root = 1
 
-        argsmodule.init(self)
-        #print(args.kill) print(kill)
-
+        argsmodule.init(self) #print(args.kill) print(kill)
         #sys.exit()
-
         PAmodule.init()
 
-        '''if root:
-            notify('foo','bar')
-        else:
-            subprocess.call(['notify-send', '--app-name=RAMer', '--icon='+appdir+'/ramerappicon.png', 'Process killed','bar'])
-        '''
-        #notifymodule.notify(self, 'Process killed','bar') sys.exit()
-
+        start = timer()
         while True:
 
+            end = timer()
+            if end > 1800:
+                start = timer()
+                dropcachesmodule.drop_caches()
+
+            count = count+1
+            if count > 1000: count = 0
             f = open("/proc/meminfo","r")
             lines = f.readlines()
-            #print(lines[1])
             kb = lines[1].split(' ')
-            mb = int(int(kb[-2])/1024) # print(mb) sys.exit()
-            #mb = 500
+            mb = int(int(kb[-2])/1024) # print(mb) sys.exit() #mb = 500
             if self.showlog: printp('RAM left: '+ str(mb) +' Mb')
-
-            #os.system("sudo -u alex ls /home/alex/0Mega/0/")
-            #os.system("ls /home/alex/0Mega/0/")
-            #os.system("sudo -u -l alex play /home/alex/0dataa/notification.mp3")
-            # subprocess.run(['play', '/home/alex/0dataa/notification.mp3'])
-
+            #print(count % 2) if count % 30 == 0:
             if debug == 0:
-                if(mb < 1000):
+                if(mb < 2000):
                     printp(chalk.yellow(mb))
                     if self.root: #print(os.environ)
                         if drop_caches_count == 0:
-                            print("sync && echo 1 > /proc/sys/vm/drop_caches && sync && echo 2 > /proc/sys/vm/drop_caches && sync && echo 3 > /proc/sys/vm/drop_caches")
-                            os.system('sync && echo 1 > /proc/sys/vm/drop_caches && sync && echo 2 > /proc/sys/vm/drop_caches && sync && echo 3 > /proc/sys/vm/drop_caches')
+                            dropcachesmodule.drop_caches()
+                            drop_caches_count+=1
+                            continue
                         drop_caches_count+=1
-                        if drop_caches_count > 10: drop_caches_count = 0
-                        #os.system('sync && sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && sync && sudo sh -c "echo 2 > /proc/sys/vm/drop_caches" && sync && sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"')
+                        print('drop_caches_count: '+ str(drop_caches_count))
+                        if drop_caches_count > 5: drop_caches_count = 0
 
             if debug == 0:
                 if(mb > self.limit):
@@ -132,20 +128,19 @@ class RAMer:
                         #print(entry.name)
                         #continue
                 #if os.path.islink(exep) and os.path.exists(exep): #print("Broken: {} -> {}".format(f, )) #print("Broken: {} -> {}".format(f, os.path.realpath(f)))
-            printp(pids)
-            #sys.exit()
+            printp(pids) #sys.exit()
 
             for pname in priority:
                 if killed:
                     continue
                 if pname in pids.keys():
                     killed = 1
-                    if kill: os.kill(pids[pname], signal.SIGTERM) #or signal.SIGKILL
+                    if self.kill: os.kill(pids[pname], signal.SIGTERM) #or signal.SIGKILL
                     notifymodule.notify(self, 'Process killed', pname)
                     printp('Killed: '+ pname)
-                    if sound: os.system("play notification.mp3 > /dev/null 2>&1")
+                    if self.sound: os.system("play notification.mp3 > /dev/null 2>&1")
 
-            time.sleep(sleep_secs)
+            time.sleep(self.sleep_secs)
 
 if __name__ == "__main__":
     ramer = RAMer()
